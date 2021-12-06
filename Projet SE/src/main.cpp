52c3f27cc37b166974dc5755ce1eb0badaab09f8
Noura <78438102+Blue-Nour@users.noreply.github.com>
2021-12-06 09:17:30 +0100

    Add files via upload

---
+++ Projet SE/src/main.cpp
@@ -0,0 +1,44 @@
+
+
+void task_BP(){
+//Lecture BP
+
+//if bp appuie
+    //Allumer led 300 ms
+    //entree dans tache lecture
+}
+
+void task_lectdata(){
+//lecture temp
+
+//lecture hum
+
+//Lecture lum
+}
+
+void task_dataProcess(){
+//Envoi temp
+
+//Envoi hum
+
+//Envoi lum
+
+//Affichage donnees
+
+}
+
+void task_lum(){
+//if lum -> sombre
+    //Allumer led
+//else
+    //eteindre led
+
+}
+
+void setup(){
+
+}
+
+void loop(){
+
+}
